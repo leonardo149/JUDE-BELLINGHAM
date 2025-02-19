06c494ab3f174f514c742f97c6953cea8e8eb5b1
leo.majamaki@gmail.com <vermin7@proton.me>
2025-02-19 16:57:34 +0200

    luotiin pelilauta

--- public/script.js
+++ public/script.js
@@ -1,6 +1,28 @@
+const BOARD_SIZE = 15;
+let board;
 
 document.getElementById('intro-screen').addEventListener('click',startGame)
 
 function startGame(){
     console.log('Klikattu')
+    document.getElementById('intro-screen').style.display = 'none';
+    document.getElementById('game-screen').style.display = 'block';
+
+    generateRandomBoard();
+}
+
+function generateRandomBoard(){
+    const newBoard = Array.from({length:BOARD_SIZE},()=>Array(BOARD_SIZE).fill(''));
+
+    for(let y = 0; y <BOARD_SIZE; y++){
+        for(let x = 0; x <BOARD_SIZE; x++){
+            if(y === 0){
+                    newBoard[y][x] = 'W';
+                }
+            }
+        }
+    
+
+    console.log(newBoard);
+
 }
